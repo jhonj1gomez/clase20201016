39e21547f9ec2ce37b19b2607613f5f00bf84430
jhonj1gomez@gmail.com <jhonj1gomez@gmail.com>
2020-11-06 17:49:58 -0500

    Adicion taller controller

---
+++ Taller20201105/app/Http/Controllers/TallerController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\controllers
+/**
+ * 
+ */
+class TallerController extends Controller
+{
+	
+	public function index()
+	{
+		echo "Hola mundo";
+		return "Hola mundo";
+	}
+}
